c03524f3bb7bc6254539f594f43f041f8be402da
Emmanuel Kiprono Davidson <kipronofb@gmail.com>
2020-04-12 12:03:39 +0300

    Updates

---
+++ precache-manifest.68026324107281b57f6e9abacd67cb35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79c1878d8fa24a63edb641e2f2bcc4d",
+    "url": "/vigilant-tribble/index.html"
+  },
+  {
+    "revision": "22b66e2f9286c0a8a9b4",
+    "url": "/vigilant-tribble/static/css/main.1d48f2e3.chunk.css"
+  },
+  {
+    "revision": "2311698faf24266a4985",
+    "url": "/vigilant-tribble/static/js/2.b32b8d12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vigilant-tribble/static/js/2.b32b8d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b66e2f9286c0a8a9b4",
+    "url": "/vigilant-tribble/static/js/main.42bf6585.chunk.js"
+  },
+  {
+    "revision": "cdd10caa4b2a04e1db65",
+    "url": "/vigilant-tribble/static/js/runtime-main.93569401.js"
+  }
+]);
